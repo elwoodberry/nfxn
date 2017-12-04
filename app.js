d7a294b76871b046f4b883d9adc8cd6787f9388e
Elwood Berry <me@elwoodberry.com>
2017-12-04 01:16:47 -0500

    Updated

--- app.js
+++ app.js
@@ -49,4 +49,8 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+// PORT
+app.set('port', process.env.PORT || 3000);
+app.listen(app.get('port'), () => {
+  console.log('Ready');
+});
